a8ccd94a18fc66e7927658280a9140172bd1c909
subbiahsura <subbiahsura@gmail.com>
2023-09-28 12:25:55 +0530

    Initial Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DonateComponent } from './donate/donate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"Home",
+    component:HomeComponent
+  },
+  {
+    path:"Donate",
+    component:DonateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
