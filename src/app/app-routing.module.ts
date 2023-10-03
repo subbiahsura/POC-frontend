25cd3ba1afdca22744e83c9ce3b390c422960b8a
saranyasneha <saranya@entrans.io>
2023-10-03 10:19:11 +0530

    login component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DonateComponent } from './donate/donate.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path:"Donate",
     component:DonateComponent
+  },
+  {
+    path:"Login",
+    component:LoginComponent
   }
 ];
 
